a783984fb34827bb7563e8e934acfed08b5bf117
mimofun <newlight12210@gmail.com>
2022-11-11 10:30:31 -0800

    country code ok

--- app/src/main/java/com/example/mimo/MainActivity.java
+++ app/src/main/java/com/example/mimo/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mimo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button phone_log,mail_log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phone_log = findViewById(R.id.phone_log);
+        mail_log = findViewById(R.id.mail_log);
+
+        phone_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent go_phon_num = new Intent(MainActivity.this,Phone_number.class);
+               startActivity(go_phon_num);
+            }
+        });
+
+
+
     }
 }
